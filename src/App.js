5911155e4769d22328708a383180825143a323e0
Akshil Thumar <akshilvthumar@gmail.com>
2021-09-13 13:02:29 +0530

    new edit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import Menu from './components/MenuComponent';
 
 function App() {
   return (
@@ -10,6 +10,7 @@ function App() {
           <NavbarBrand herf="/">newapp</NavbarBrand>
         </div>
       </Navbar>
+      <Menu/>
     </div>
   );
 }
